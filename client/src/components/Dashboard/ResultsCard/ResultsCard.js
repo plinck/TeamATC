23abde838653748ab4a3964b891ce91c6867aa28
Paul Linck <paul.linck@gmail.com>
2020-02-19 12:06:43 -0500

    Got header rows workign on both, need to get typography better

--- client/src/components/Dashboard/ResultsCard/ResultsCard.js
+++ client/src/components/Dashboard/ResultsCard/ResultsCard.js
@@ -12,29 +12,7 @@ const ResultsCard = (props) => {
 
     return (
         <div>
-            {/*<-- Leaderboad -->*/}
-            {/*
-                            userOrTeamUid: null,
-                            userOrTeamName: "",
-                            isThisMine: false,
-                            distanceTotal : 0,
-                            pointsTotal : 0,
-                            swimDistanceTotal : 0,
-                            swimPointsTotal : 0,
-                            bikeDistanceTotal : 0,
-                            bikePointsTotal : 0,
-                            runDistanceTotal : 0,
-                            runPointsTotal : 0,
-                            durationTotal: 0,
-                            nbrActivities: 0,
-                            swimNbrActivities: 0,
-                            swimDurationTotal: 0,
-                            bikeNbrActivities: 0,
-                            bikeDurationTotal: 0,
-                            runNbrActivities: 0,
-                            runDurationTotal: 0
-            */}
-
+            {/*<-- Leaderboad row -->*/}
             {/* One Row for each result in leaderboard */}
             <div className="row">
                 <div className="col s1 m1">
@@ -47,16 +25,16 @@ const ResultsCard = (props) => {
                     {props.result.userOrTeamName}
                 </div>
                 <div className="black-text col m2 m2 truncate">
-                    {props.result.pointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Points
+                    {props.result.pointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
                 <div className="blue-text col m2 m2 truncate">
-                    {props.result.swimPointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Points
+                    {props.result.swimPointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
                 <div className="red-text col m2 m2 truncate">
-                    {props.result.bikePointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Points
+                    {props.result.bikePointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
                 <div className="green-text col m2 m2 truncate">
-                    {props.result.runPointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Points
+                    {props.result.runPointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
             </div>
             {/* End Row */}         
