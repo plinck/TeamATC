02c2378d3373e029a4efaca3fc2d968f257c6d1e
Paul Linck <paul.linck@gmail.com>
2020-02-19 19:06:57 -0500

    Added index to props to delineate records of results

--- client/src/components/Dashboard/ResultsCard/ResultsCard.js
+++ client/src/components/Dashboard/ResultsCard/ResultsCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
 
 import './ResultsCard.css'
 
@@ -8,13 +9,21 @@ const ResultsCard = (props) => {
         return(null);
     }
 
-    console.log(`${JSON.stringify(props.result)}`);
+    //console.log(`${JSON.stringify(props.result)}`);
+
+    // Band odd rows for clarity
+    let rowBg = "";
+    let rowFg = ""
+    if (props.index % 2 !== 0) {
+        rowBg = "info.main";
+        rowFg = "white";
+    }
 
     return (
         <div>
             {/*<-- Leaderboad row -->*/}
             {/* One Row for each result in leaderboard */}
-            <div className="row">
+            <Box className="row" color={rowFg} bgcolor={rowBg} m={0}>
                 <div className="col s1 m1">
                     { props.result.isThisMine ?
                         <img style={{maxHeight: '20px'}} src={"/images/me.png"} alt={"me"} />
@@ -24,19 +33,19 @@ const ResultsCard = (props) => {
                 <div className="col s3 m3 truncate">
                     {props.result.userOrTeamName}
                 </div>
-                <div className="black-text col m2 m2 truncate">
+                <div className="col m2 m2 truncate">
                     {props.result.pointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
-                <div className="blue-text col m2 m2 truncate">
+                <div className="col m2 m2 truncate">
                     {props.result.swimPointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
-                <div className="red-text col m2 m2 truncate">
+                <div className="col m2 m2 truncate">
                     {props.result.bikePointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
-                <div className="green-text col m2 m2 truncate">
+                <div className="col m2 m2 truncate">
                     {props.result.runPointsTotal.toFixed(0).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}
                 </div>
-            </div>
+            </Box>
             {/* End Row */}         
         </div>
     )
