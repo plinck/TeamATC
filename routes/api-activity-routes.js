107888e45e69d3f473d3716f0e124ae28f427d93
Paul Linck <paul.linck@gmail.com>
2020-02-21 06:07:01 -0500

    Quick fix on activity routes

--- routes/api-activity-routes.js
+++ routes/api-activity-routes.js
@@ -1,6 +1,7 @@
 const admin = require("../middleware/authServerCommon");
 const requiresLogin = require('../middleware/requiresLogin.js');
 const db = admin.firestore();;
+const {ORG, ENV, USERS_DB} = require("../ServerEnvironment");
 
 // Static *class* variables
 let activities = []
