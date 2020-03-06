66999e5f5c7b8624804c3b8d12cf4170f0dc5535
Noah A <nanderson815@gmail.com>
2020-03-05 20:17:42 -0500

    updating navbar

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -1,146 +1,166 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import M from "materialize-css/dist/js/materialize.min.js";
 import SignOutButton from '../Auth/SignOut/SignOut';
+import AppBar from '@material-ui/core/AppBar';
+import { makeStyles } from '@material-ui/core/styles';
 import { withAuthUserContext } from '../Auth/Session/AuthUserContext';
 import { withRouter } from 'react-router-dom';
 import AccountMenu from "../Account/AccountMenu";
 import './Navigation.css';
+import { Button, Typography, Container, Toolbar, IconButton, SwipeableDrawer } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
-class Navigation extends React.Component {
-  state = {
-    isTop: true
-  }
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    color: 'white',
+    textDecoration: 'none'
+  },
+  title: {
+    flexGrow: 1,
+  },
+  titleText: {
+    verticalAlign: "super",
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+  navIconHide: {
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+  navButtonsHide: {
+    [theme.breakpoints.down('sm')]: {
+      display: 'none',
+    },
+  },
+}));
 
-  componentDidMount() {
-    let elem = document.querySelector(".sidenav");
-    M.Sidenav.init(elem, {
-      edge: "left",
-      inDuration: 250,
-      closeOnClick: true
-    });
+let Navigation = (props) => {
+  const classes = useStyles();
+  let name = 'TeamATC Challenge';
 
-    document.addEventListener('scroll', () => {
-      const isTop = window.scrollY < 100;
-      if (isTop !== this.state.isTop) {
-        // console.log(isTop);
-        this.setState({ isTop })
-      }
-    });
-  }
+  const [state, setState] = React.useState({
+    left: false,
+  });
 
-  forceCloseSideNav() {
-    let elem = document.querySelector(".sidenav");
-    let instance = M.Sidenav.getInstance(elem);
-
-    instance.close();
-  }
-
-  render() {
-
-    let navBarClass = '';
-    let name = '';
-
-    if (this.state.isTop && this.props.history.location.pathname === "/") {
-      navBarClass = 'transparent z-depth-0';
-    } else {
-      navBarClass = 'z-depth-0 blue darken-4'
-      name = 'TeamATC Challenge'
+  const toggleDrawer = (side, open) => event => {
+    if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
     }
 
+    setState({ ...state, [side]: open });
+  };
 
-    const navigationAdminMobile =
-      <ul>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/dashboard">Home</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/activities">Activities</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/account">Account</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/admin">Admin</NavLink></li>
-        {/* this.props.user.isModerator ? <li><NavLink onClick={this.forceCloseSideNav} to="/moderator">Moderator</NavLink></li> : "" */}
-        <li onClick={this.forceCloseSideNav}><SignOutButton /></li>
-        </ul>
-        ;
-        
-        const navigationAdmin =
-        <ul>
-        <li><NavLink to="/dashboard">Home</NavLink></li>
-        <li><NavLink to="/activities">Activities</NavLink></li>
-        <li><NavLink to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
-        <li><AccountMenu /></li>
-        <li><NavLink to="/admin">Admin</NavLink></li>
-        </ul>
-        ;
-        
-        const navigationAuthMobile =
-        <ul>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/dashboard">Home</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/activities">Activities</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/account">Account</NavLink></li>
-        <li onClick={this.forceCloseSideNav}> <SignOutButton /></li>
-        </ul>
-        ;
-        
-        const navigationAuth =
-        <ul>
-        <li><NavLink to="/dashboard">Home</NavLink></li>
-        <li><NavLink to="/activities">Activities</NavLink></li>
-        <li><NavLink to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
-        <li><AccountMenu /></li>
-        </ul>
-      ;
 
-    const navigationNonAuthMobile =
-      <ul>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/">Landing</NavLink></li>
-        <li><NavLink onClick={this.forceCloseSideNav} to="/signin">Sign In</NavLink></li>
+  const navigationAdminMobile =
+    <ul>
+      <li><NavLink to="/dashboard">Home</NavLink></li>
+      <li><NavLink to="/activities">Activities</NavLink></li>
+      <li><NavLink to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
+      <li><NavLink to="/account">Account</NavLink></li>
+      <li><NavLink to="/admin">Admin</NavLink></li>
+      {/* this.props.user.isModerator ? <li><NavLink onClick={this.forceCloseSideNav} to="/moderator">Moderator</NavLink></li> : "" */}
+      <li><SignOutButton /></li>
     </ul>
-      ;
+    ;
 
-    const navigationNonAuth =
-      <ul>
-        <li><NavLink to="/">Landing</NavLink></li>
-        <li><NavLink to="/signin">Sign In</NavLink></li>
-      </ul>
-      ;
+  const navigationAdmin =
+    <ul>
+      <li><NavLink to="/dashboard">Home</NavLink></li>
+      <li><NavLink to="/activities">Activities</NavLink></li>
+      <li><NavLink to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
+      <li><AccountMenu /></li>
+      <li><NavLink to="/admin">Admin</NavLink></li>
+    </ul>
+    ;
 
-    // get auth user from react-context firebase
-    // Not the AuthUSerContext Provider passes the authUser
-    // in its value={} paramater (see withAuthentication component in Auth/Session)
-    // ANY COMPONENT that needs authUser info uses consumer this way
+  const navigationAuthMobile =
+    <ul>
+      <li><NavLink to="/dashboard">Home</NavLink></li>
+      <li><NavLink to="/activities">Activities</NavLink></li>
+      <li><NavLink to="/activitypage" className="waves-effect waves-light btn">New Workut</NavLink></li>
+      <li><NavLink to="/account">Account</NavLink></li>
+      <li > <SignOutButton /></li>
+    </ul>
+    ;
 
-    let navBar, navBarMobile;
-    if (this.props.user.authUser && this.props.user.isAdmin) {
-      navBar = navigationAdmin;
-      navBarMobile = navigationAdminMobile;
-    } else if (this.props.user.authUser) {
-      navBar = navigationAuth;
-      navBarMobile = navigationAuthMobile;
-    } else {
-      navBar = navigationNonAuth;
-      navBarMobile = navigationNonAuthMobile;
-    }
 
-    return (
-      <div>
-        <ul className="sidenav" id="mobile-menu">
-            {navBarMobile}
-        </ul>
 
-        <div className='navbar-fixed'>
-          <nav className={navBarClass}>
-            <div className="container nav-wrapper">
-              <a href="/" className="brand-logo"><img className="logo" src="../images/logo.png" alt="TeamATC Challenge Logo"/> <span>{name}</span></a>
-              <a href="#!" data-target="mobile-menu" className="sidenav-trigger"><i className="material-icons">menu</i></a>
-              <ul className="right hide-on-med-and-down">
-                {navBar}
-              </ul>
-            </div>
-          </nav>
-        </div>
-      </div>
-    );
-  }// render
+
+  const navigationAuth =
+    <>
+      <Button><NavLink className={classes.menuButton} to="/dashboard">Home</NavLink></Button>
+      <Button><NavLink className={classes.menuButton} to="/activities">Activities</NavLink></Button>
+      <Button variant="contained" color="secondary"><NavLink className={classes.menuButton} to="/activitypage" >New Workut</NavLink></Button>
+      <Button><AccountMenu /></Button>
+    </>
+    ;
+
+  const navigationNonAuthMobile =
+    <ul>
+      <li><NavLink to="/">Landing</NavLink></li>
+      <li><NavLink to="/signin">Sign In</NavLink></li>
+    </ul>
+    ;
+
+  const navigationNonAuth =
+    <div className={classes.navButtonsHide}>
+      <Button><NavLink className={classes.menuButton} to="/">Landing</NavLink></Button>
+      <Button><NavLink className={classes.menuButton} to="/signin">Sign In</NavLink></Button>
+    </div>
+
+  // get auth user from react-context firebase
+  // Not the AuthUSerContext Provider passes the authUser
+  // in its value={} paramater (see withAuthentication component in Auth/Session)
+  // ANY COMPONENT that needs authUser info uses consumer this way
+
+  let navBar, navBarMobile;
+  if (props.user.authUser && props.user.isAdmin) {
+    navBar = navigationAdmin;
+    navBarMobile = navigationAdminMobile;
+  } else if (props.user.authUser) {
+    navBar = navigationAuth;
+    navBarMobile = navigationAuthMobile;
+  } else {
+    navBar = navigationNonAuth;
+    navBarMobile = navigationNonAuthMobile;
+  }
+
+  return (
+    <div>
+      <SwipeableDrawer
+        open={state.left}
+        onClose={toggleDrawer('left', false)}
+        onOpen={toggleDrawer('left', true)}
+      >
+        {navBarMobile}
+      </SwipeableDrawer>
+
+      <AppBar>
+        <Container>
+          <Toolbar>
+            <Typography variant="h4" className={classes.title}>
+              <img className="logo" src="../images/logo.png" alt="TeamATC Challenge Logo" /> <span className={classes.titleText}>{name}</span>
+            </Typography>
+            {navBar}
+            <IconButton
+              color="inherit"
+              aria-label="Open drawer"
+              onClick={toggleDrawer('left', true)}
+              className={classes.navIconHide}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Toolbar>
+        </Container>
+      </AppBar >
+
+    </div >
+  );
 } //class
 
 export default withAuthUserContext(withRouter(Navigation));
