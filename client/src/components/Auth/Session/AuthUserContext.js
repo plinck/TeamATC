e2610caa95394964a9f2f93333d6c263d3bbb891
Paul Linck <paul.linck@gmail.com>
2020-03-24 14:58:28 -0400

    AuthContext

--- client/src/components/Auth/Session/AuthUserContext.js
+++ client/src/components/Auth/Session/AuthUserContext.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 const AuthUserContext = React.createContext(null);
 
+// This is the higher order component used in any component that needs auth / session info
 const withAuthUserContext = Component => props => (
   <AuthUserContext.Consumer>
     {user => <Component {...props} user={user} />}
