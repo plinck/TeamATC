2a9e23bd2f96628df4603b18f574a7baa933a1bb
Noah A <nanderson815@gmail.com>
2020-03-26 17:37:23 -0400

    working on cleaning up activities

--- client/src/components/Activity/Activities.js
+++ client/src/components/Activity/Activities.js
@@ -2,30 +2,32 @@ import React from 'react';
 import { withRouter } from 'react-router-dom';
 import { Redirect } from 'react-router';
 import { Link } from 'react-router-dom';
-
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Box from "@material-ui/core/Box";
-import { Button } from '@material-ui/core';
-
-// For select input field
-import FormControl from '@material-ui/core/FormControl';
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import Select from "@material-ui/core/Select";
+import { ButtonGroup, Grid, CircularProgress, Box, Button, Container, FormControl, InputLabel, MenuItem, Select, Typography, Tooltip, IconButton } from '@material-ui/core'
+import AddIcon from '@material-ui/icons/Add';
+import GetAppIcon from '@material-ui/icons/GetApp';
+import PoolIcon from '@material-ui/icons/Pool';
+import DirectionsBikeIcon from '@material-ui/icons/DirectionsBike';
+import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 
 import { withAuthUserContext } from "../Auth/Session/AuthUserContext";
 import Activity from './Activity';
 import ActivityDB from './ActivityDB';
 
 import { withStyles } from '@material-ui/core/styles';
-
-// export csv functionality
 // eslint-disable-next-line no-unused-vars
 import { CSVLink, CSVDownload } from "react-csv";
 
 const styles = theme => ({
-    root: {
+    act: {
         width: '100%',
+        overflow: "auto",
+        height: "77vh"
+    },
+    root: {
+        [theme.breakpoints.up('md')]: {
+            marginLeft: "57px",
+        },
+        paddingTop: "10px"
     },
     heading: {
         fontSize: theme.typography.pxToRem(15),
@@ -39,10 +41,9 @@ const styles = theme => ({
         flexWrap: 'wrap',
     },
     formControl: {
-        marginLeft: theme.spacing.unit,
-        marginRight: theme.spacing.unit,
-        minWidth: 200
-      },
+        margin: theme.spacing(1),
+        minWidth: 120,
+    },
     inputFix: {
         marginTop: 50
     },
@@ -51,9 +52,9 @@ const styles = theme => ({
         marginRight: theme.spacing.unit,
         width: 200,
     },
-        //style for font size
-    resize:{
-        fontSize:200
+    //style for font size
+    resize: {
+        fontSize: 200
     }
 });
 
@@ -81,14 +82,14 @@ class Activities extends React.Component {
         if (!filterByString) {
             if (this.props.filterByString) {
                 filterByString = this.props.filterByString
-            } 
+            }
         }
 
         if (this.props.activities) {
-            this.setState({ activities: this.props.activities });   
+            this.setState({ activities: this.props.activities });
         } else {
             if (filterByString) {
-                this.setState({filterByString: filterByString, filterBy: filterByString});
+                this.setState({ filterByString: filterByString, filterBy: filterByString });
                 this.getFilteredActivities(filterByString);
             } else {
                 this.getFilteredActivities();
@@ -110,11 +111,11 @@ class Activities extends React.Component {
             filterValue: undefined
         }
 
-        switch(filterByString) {
+        switch (filterByString) {
             case "All":
                 filterObj = undefined;
                 break;
-            case "Team":                            
+            case "Team":
                 filterObj.filterName = "teamUid";
                 filterObj.filterValue = this.props.user.teamUid;
                 break;
@@ -125,33 +126,33 @@ class Activities extends React.Component {
             default:
                 filterObj.filterName = "uid";
                 filterObj.filterValue = this.props.user.uid;
-            }     
+        }
 
-        this.setState({isLoading: true});
+        this.setState({ isLoading: true });
         ActivityDB.getFiltered(filterObj, undefined, lastActivityDoc)
-        .then(res => {
-            let activities = res.activities;
-            this.setState({ activities: activities, lastActivityDoc: res.lastActivityDoc, isLoading: false});
-        }).catch(err => {
-            this.setState({isLoading: false});
-            console.error(err);
-        });
+            .then(res => {
+                let activities = res.activities;
+                this.setState({ activities: activities, lastActivityDoc: res.lastActivityDoc, isLoading: false });
+            }).catch(err => {
+                this.setState({ isLoading: false });
+                console.error(err);
+            });
     };
 
     // Get next page
     getNextPage = () => {
-       let filterBy = this.state.filterBy;
-       let lastActivityDoc = this.state.lastActivityDoc;
-       let filterObj = {
+        let filterBy = this.state.filterBy;
+        let lastActivityDoc = this.state.lastActivityDoc;
+        let filterObj = {
             filterName: undefined,
             filterValue: undefined
-       }
+        }
 
-        switch(filterBy) {
+        switch (filterBy) {
             case "All":
                 filterObj = undefined;
                 break;
-            case "Team":                            
+            case "Team":
                 filterObj.filterName = "teamUid";
                 filterObj.filterValue = this.props.user.teamUid;
                 break;
@@ -161,17 +162,17 @@ class Activities extends React.Component {
                 break;
             default:
                 filterObj = undefined;
-            }     
+        }
 
-        this.setState({isLoading: true});
+        this.setState({ isLoading: true });
         ActivityDB.getFiltered(filterObj, undefined, lastActivityDoc)
-        .then(res => {
-            let activities = res.activities;
-            this.setState({ activities: activities, lastActivityDoc: res.lastActivityDoc, isLoading: false});
-        }).catch(err => {
-            this.setState({isLoading: false});
-            console.error(err);
-        });
+            .then(res => {
+                let activities = res.activities;
+                this.setState({ activities: activities, lastActivityDoc: res.lastActivityDoc, isLoading: false });
+            }).catch(err => {
+                this.setState({ isLoading: false });
+                console.error(err);
+            });
     };
 
     // Get acvities in certain order 
@@ -185,8 +186,8 @@ class Activities extends React.Component {
             orderValue: undefined
         }
 
-        switch(orderBy) {
-            case "Team":                            
+        switch (orderBy) {
+            case "Team":
                 orderObj.orderName = "teamName";
                 orderObj.orderValue = undefined;       // Ascending - "desc" is alternative
                 break;
@@ -196,35 +197,35 @@ class Activities extends React.Component {
                 break;
             default:
                 orderObj = undefined;
-            }     
+        }
 
         // Get with security
         // comes back sorted already
-        this.setState({isLoading: true});
+        this.setState({ isLoading: true });
         ActivityDB.getOrdered(orderObj)
             .then(res => {
                 let activities = res;
-                this.setState({ activities: activities, isLoading: false});
+                this.setState({ activities: activities, isLoading: false });
             })
             .catch(err => {
-                this.setState({isLoading: false});
+                this.setState({ isLoading: false });
                 console.error(err);
             });
     };
 
     // Delete this activity
     activityDelete = (id) => {
-        ActivityDB.delete( id )
-        .then(res => {
-            console.log("Deleted activity");
-            this.getFilteredActivities();
-        })
-        .catch(err => {
-            alert(err);
-            console.error(err); 
-        });
+        ActivityDB.delete(id)
+            .then(res => {
+                console.log("Deleted activity");
+                this.getFilteredActivities();
+            })
+            .catch(err => {
+                alert(err);
+                console.error(err);
+            });
     }
-    
+
     // ************************************************************
     // Form Change Handlers - try to keeo grouped    
     // ************************************************************
@@ -245,7 +246,7 @@ class Activities extends React.Component {
                 });
                 this.getOrderedActivities(event.target.value);
                 break;
-            case "searchBy": 
+            case "searchBy":
                 this.setState({
                     [event.target.name]: event.target.value,
                 });
@@ -273,37 +274,101 @@ class Activities extends React.Component {
             console.error("Fatal Error")
             return (<div> <p>FATAL ERROR Gettng activities ...</p> </div>)
         }
-        if (this.state.activities === null || this.state.isLoading) {
-            return (<div> <CircularProgress className={classes.progress} /> <p>Loading ...</p> </div>)
-        } 
+        if (this.state.activities === null) {
+            return (<Grid container style={{ marginTop: '10px' }} justify="center"><CircularProgress /> <p>Loading ...</p> </Grid>)
+
+        }
 
         // Search and filter
         //let searchBy = this.state.searchBy;
         let filterBy = this.state.filterBy;
         //let orderBy = this.state.orderBy;
         let activities = this.state.activities;
-        
-        const sortFilterRow = 
-            <Box className="row"  border={1} m={1} p={0}>
-                <FormControl className={`${classes.formControl} col s3 m3`} variant="filled">
-                    <InputLabel id="filterByLabel">Filter By</InputLabel>
-                    <Select
-                        labelId="filterByLabel"
-                        id="filterBy"
-                        value={filterBy}
-                        name="filterBy"
-                        type="text"
-                        margin="normal"
-                        className={classes.textField}
-                        style={{marginTop: 23, marginBottom: 16, padding: 0}}
-                        onChange={this.handleChange}
-                        >
-                        <MenuItem value={"Mine"}>Mine</MenuItem>
-                        <MenuItem value={"Team"}>Team</MenuItem>
-                        <MenuItem value={"All"}>All</MenuItem>
-                    </Select>
-                </FormControl>
 
+        const sortFilterRow =
+            <Grid container>
+                <Grid
+                    container
+                    item
+                    xs={12}
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center">
+                    <Grid item xs={6}>
+                        <Typography variant="h4">
+                            Activities
+                    </Typography>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={6}
+                        style={{ textAlign: "right" }}>
+                        <Link to="/activityform" style={{ color: 'grey', textDecoration: "none" }}>
+                            <Tooltip title="New Activity">
+                                <Button color="default" startIcon={<AddIcon />}>
+                                    New Activity
+                                </Button>
+                            </Tooltip>
+                        </Link>
+                        <CSVLink
+                            data={activities}
+                            filename={'teamatc-transactions.csv'}
+                            target="_blank">
+                            <Button color="default" startIcon={<GetAppIcon />}>
+                                Export CSV
+                                </Button>
+                        </CSVLink>
+                    </Grid>
+                </Grid>
+                <Grid
+                    container
+                    item
+                    xs={12}
+                    direction="row"
+                    justify="space-between"
+                    alignItems="center">
+                    <Grid item xs={12} md={6}>
+                        <FormControl className={classes.formControl}>
+                            <InputLabel id="filterByLabel">Filter By</InputLabel>
+                            <Select
+                                labelId="filterByLabel"
+                                id="filterBy"
+                                name="filterBy"
+                                value={filterBy}
+                                onChange={this.handleChange}
+                                label="Filter By"
+                            >
+                                <MenuItem value={"Mine"}>Mine</MenuItem>
+                                <MenuItem value={"Team"}>Team</MenuItem>
+                                <MenuItem value={"All"}>All</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={6}
+                        style={{ textAlign: "right" }}>
+                        <ButtonGroup color="primary">
+                            <Tooltip title="All">
+                                <Button variant="contained" >All</Button>
+                            </Tooltip>
+                            <Tooltip title="Swim">
+                                <Button><PoolIcon /></Button>
+                            </Tooltip>
+                            <Tooltip title="Bike">
+                                <Button><DirectionsBikeIcon /></Button>
+                            </Tooltip>
+                            <Tooltip title="Run">
+                                <Button><DirectionsRunIcon /></Button>
+                            </Tooltip>
+                        </ButtonGroup>
+                    </Grid>
+                    {/* <Button onClick={this.getNextPage}>Page
+                        <i style={{ cursor: 'pointer', marginTop: 5, marginRight: 1 }}
+                            className="material-icons indigo-text text-darken-4">skip_next
+                        </i>
+                    </Button> */}
+                </Grid>
                 {/*  Get rid of order by for now.  
                 <FormControl variant="filled" className={`${classes.formControl} col s3 m3`}>
                     <InputLabel id="orderByLabel">Order By</InputLabel>
@@ -350,65 +415,39 @@ class Activities extends React.Component {
                         />
                 </div>
                 */}
-                
-                <div className="col s2 m2">
-                    <Link to="/activityform">
-                        <i style={{cursor: 'pointer', marginTop: 5, marginRight: 1}}
-                            className="material-icons indigo-text text-darken-4">add
-                        </i>{" "}
-                    </Link>
-                    <CSVLink
-                        data={activities}
-                        filename={'teamatc-transactions.csv'}
-                        target="_blank">
-                        <i style={{cursor: 'pointer', marginTop: 5, marginRight: 1}}
-                            className="material-icons indigo-text text-darken-4">system_update_alt
-                        </i>
-                    </CSVLink> 
-                    <Button onClick={this.getNextPage}>Page
-                        {/*
-                            <i style={{cursor: 'pointer', marginTop: 5, marginRight: 1}}
-                            className="material-icons indigo-text text-darken-4">skip_previous
-                        </i>
-                        */}
-                        <i style={{cursor: 'pointer', marginTop: 5, marginRight: 1}}
-                            className="material-icons indigo-text text-darken-4">skip_next
-                        </i>
-                    </Button>
-                </div>
-            </Box>
+            </Grid >
 
-        const headerRow = 
-        <Box className="row"  fontStyle="oblique" fontWeight="fontWeightBold" border={1} m={0}>
-            <div className="col s1 m1">
+        const headerRow =
+            <Box className="row" fontStyle="oblique" fontWeight="fontWeightBold" border={1} m={0}>
+                <div className="col s1 m1">
+                </div>
+                <div className="col s4 m2 truncate">
+                    Team
             </div>
-            <div className="col s4 m2 truncate">
-                Team
+                <div className="black-text col s4 m2 truncate">
+                    Athlete
             </div>
-            <div className="black-text col s4 m2 truncate">
-                Athlete
+                <div className="blue-text col s3 m2 truncate">
+                    Date
             </div>
-            <div className="blue-text col s3 m2 truncate">
-                Date
+                <div className="red-text col s4 m2 truncate">
+                    Name
             </div>
-            <div className="red-text col s4 m2 truncate">
-                Name
+                <div className="green-text col s4 m1 truncate">
+                    Hrs
             </div>
-            <div className="green-text col s4 m1 truncate">
-                Hrs
+                <div className="green-text col s4 m1 truncate">
+                    Distance
             </div>
-            <div className="green-text col s4 m1 truncate">
-                Distance
-            </div>
-        </Box>
+            </Box>
 
         if (this.props.user.authUser) {
             // Conditional rendering
-            let activityView = 
+            let activityView =
                 <div>
                     {sortFilterRow}
                     {headerRow}
-                    <div className={classes.root}>
+                    <div className={classes.act}>
                         {activities.map((activity, index) => {
                             return (
                                 <div key={activity.id}>
@@ -418,13 +457,15 @@ class Activities extends React.Component {
                                     />
                                 </div>
                             );
-                        })} 
+                        })}
                     </div>
                 </div>
 
             return (
-                <div className="container">
-                    {activityView}
+                <div style={{ backgroundColor: "#f2f2f2" }} className={classes.root}>
+                    <Container maxWidth="xl">
+                        {activityView}
+                    </Container>
                 </div>
             );
         } else {
