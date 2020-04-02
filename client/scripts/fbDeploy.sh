2e7dbace3a05ffc722cd75a548c1b433e975e09f
Paul Linck <paul.linck@gmail.com>
2020-04-02 15:48:25 -0400

    Added cloud batch update for activities and called from client

--- client/scripts/fbDeploy.sh
+++ client/scripts/fbDeploy.sh
@@ -1,4 +1,7 @@
-cd ~/GitHub/firebase/TeamATC/client && npm run build
 # Set Environment variables - no need - get from client
 # firebase functions:config:set environment.org="ATC" environment.env="dev"
+
+# copy prod env to ensure deplaying prod
+cp ~/GitHub/firebase/TeamATC/client/.env.prod ~/GitHub/firebase/TeamATC/client/.env
+cd ~/GitHub/firebase/TeamATC/client && npm run build
 firebase deploy --project teamatc-challenge
--- client/src/components/Team/TeamAPI.js
+++ client/src/components/Team/TeamAPI.js
@@ -4,15 +4,24 @@ class TeamAPI {
     // Cloud Functions
     static updateUserAndActivityTeams(challengeUid, team) {
         const firebase = Util.getFirebaseAuth();
-        const fBFupdateTeam = firebase.functions.httpsCallable('fBFupdateTeam');
         const request = {challengeUid: challengeUid, team: team};
         
+        const fBFupdateTeam = firebase.functions.httpsCallable('fBFupdateTeam');
         fBFupdateTeam(request).then( (res) => {
             // Read result of the Cloud Function.
             console.log(`updated users with team.id ${team.id} to team name: ${team.name} from cloud function`);
         }).catch(err => {
             console.error(`${err}`);
         });
+
+        const fBFupdateActivityTeamName = firebase.functions.httpsCallable('fBFupdateActivityTeamName');
+        fBFupdateActivityTeamName(request).then( (res) => {
+            // Read result of the Cloud Function.
+            console.log(`updated activities with team.id ${team.id} to team name: ${team.name} from cloud function`);
+        }).catch(err => {
+            console.error(`${err}`);
+        });
+
     }
 
 }
