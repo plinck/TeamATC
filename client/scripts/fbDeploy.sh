1c1398cd668bd8e07ccec4d9109c97f1683fa697
Paul Linck <paul.linck@gmail.com>
2020-05-07 07:27:21 -0400

    fixed scripts and pckage jso for deploying

--- client/scripts/fbDeploy.sh
+++ client/scripts/fbDeploy.sh
@@ -5,4 +5,6 @@ cd ~/GitHub/firebase/TeamATC/client
 sudo npm run build
 firebase functions:config:unset env
 firebase functions:config:set env="$(cat ~/GitHub/firebase/TeamATC/client/functions/.env-strava-config.json)"
-sudo firebase deploy
+#sudo is needed if permissions / ownership ins messed up in repo =- use chown to fix
+#sudo firebase deploy
+firebase deploy
