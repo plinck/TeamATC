34f8c20aa3070934000173a14e02461731ec5f69
Noah Anderson <noahanderson@noahs-mbp.lan>
2020-06-07 11:40:28 -0400

    cleaned up stytles

--- client/src/components/Dashboard/GoogleMap/CustomMarker/CustomMarker.js
+++ client/src/components/Dashboard/GoogleMap/CustomMarker/CustomMarker.js
@@ -1,7 +1,40 @@
 import React from "react";
+import { makeStyles, createStyles, Theme } from "@material-ui/core/styles";
 import { Avatar, Card, CardContent, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles(() =>
+  createStyles({
+    card: {
+      background: "white",
+      width: "150px",
+      top: "-125px",
+      right: "-75px",
+      position: "absolute",
+      zIndex: "500",
+    },
+    avatar: {
+      left: "-23px",
+      top: "-51px",
+      border: "3px solid #ff0100",
+      zIndex: "200",
+    },
+    carrot: {
+      position: "absolute",
+      zIndex: "199",
+      bottom: "-1px",
+      left: "-10px",
+      borderWidth: "10px 10px 0px",
+      borderStyle: "solid",
+      borderColor: "#ff0100 transparent",
+      display: "block",
+      width: "0px",
+    },
+  })
+);
+
 export default function CustomMarker({ name, photo, $hover, label }) {
+  const classes = useStyles();
+
   let initials = name.match(/\b\w/g) || [];
   initials = ((initials.shift() || "") + (initials.pop() || "")).toUpperCase();
 
@@ -10,16 +43,7 @@ export default function CustomMarker({ name, photo, $hover, label }) {
   return (
     <div>
       {$hover ? (
-        <Card
-          style={{
-            background: "white",
-            width: "150px",
-            top: "-125px",
-            right: "-75px",
-            position: "absolute",
-            zIndex: "500",
-          }}
-        >
+        <Card className={classes.card}>
           <CardContent style={{ padding: "3px" }}>
             <Typography
               variant="h5"
@@ -30,38 +54,16 @@ export default function CustomMarker({ name, photo, $hover, label }) {
             <Typography variant="body2" color="textSecondary" component="p">
               Dist: {label.distance} miles
             </Typography>
-            <Typography
-              variant="body2"
-              color="textSecondary"
-              component="p"
-            >
+            <Typography variant="body2" color="textSecondary" component="p">
               {label.complete}% complete
             </Typography>
           </CardContent>
         </Card>
       ) : null}
-      <Avatar
-        style={{
-          left: "-23px",
-          top: "-51px",
-          border: "3px solid #ff0100",
-          zIndex: "200",
-        }}
-        src={photo ? photo : null}
-      >{photo ? null : initials}</Avatar>
-      <div
-        style={{
-          position: "absolute",
-          zIndex: "199",
-          bottom: "-1px",
-          left: "-10px",
-          borderWidth: "10px 10px 0px",
-          borderStyle: "solid",
-          borderColor: "#ff0100 transparent",
-          display: "block",
-          width: "0px",
-        }}
-      />
+      <Avatar className={classes.avatar} src={photo ? photo : null}>
+        {photo ? null : initials}
+      </Avatar>
+      <div className={classes.carrot} />
     </div>
   );
 }
