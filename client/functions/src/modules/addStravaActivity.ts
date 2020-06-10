8a25fa540a088cf6191f92dc1d4ab48baa7a8de6
Paul Linck <paul.linck@gmail.com>
2020-06-10 13:20:03 -0400

    Added strava activity to user/activies and challenge (if valid dates)

--- client/functions/src/modules/addStravaActivity.ts
+++ client/functions/src/modules/addStravaActivity.ts
@@ -1,56 +1,67 @@
-import * as admin from 'firebase-admin';
-import { APP_CONFIG } from "./FirebaseEnvironment";
+import { ChallengeDB } from "./db/ChallengeDB";
+import { Challenge } from "./Interfaces/Challenge"; 
+import { Activity } from './interfaces/Activity';
+import { User } from './interfaces/User';
+import ActivityDB from './db/ActivityDB';
 
 // ===============================================================
 // Local - non-exported functions
 // ===============================================================
-const addStravaActivity = ((user : any, stravaActivity : any) => {
-    
-    let stravaActivityDistanceUnits = "Miles";
-    let stravaActivityDistance = 0.0;
-    let stravaActivityType = "";
-    console.log(`Strava Activity. Type: ${stravaActivity.type.toLowerCase()}, id: ${stravaActivity.id}`);
-    console.log(`addStravaActivity. Athlete Id ${user.stravaAthleteId},displayName: ${user.displayName},challengeUid: ${user.challengeUid},teamName: ${user.teamName},strava ActivyId: ${stravaActivity.id},Type: ${stravaActivity.type.toLowerCase()},Time: ${new Date(stravaActivity.start_date)}`);
-
-    switch (stravaActivity.type.toLowerCase()) {
-        case "swim":
-            stravaActivityType = "Swim";
-            stravaActivityDistanceUnits = "Yards";
-            //convert meters to yards
-            stravaActivityDistance = stravaActivity.distance * 1.09361;
-            break;
-        case "ride":
-            stravaActivityType = "Bike";
-            // meters to miles
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-            break;
-        case "virtualride":
-            stravaActivityType = "Bike";
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-            break;
-        case "handcycle":
-            stravaActivityType = "Bike";
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-            break;
-        case "run":
-            stravaActivityType = "Run";
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-            break;
-        case "virtualrun":
-            stravaActivityType = "Run";
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-            break;
-        case "walk":
-            stravaActivityType = "Run";
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-            break;
-        default:
-            stravaActivityType = "Other";
-            stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
-        }
-    stravaActivityDistance = Math.round((stravaActivityDistance + Number.EPSILON) * 100) / 100
+const addStravaActivity = ((user: User, stravaActivity: any) => {
     return new Promise((resolve, reject) => {
-        const activity = {
+
+        let stravaActivityDistanceUnits = "Miles";
+        let stravaActivityDistance = 0.0;
+        let stravaActivityType = "";
+        console.log(`Strava Activity. Type: ${stravaActivity.type.toLowerCase()}, id: ${stravaActivity.id}`);
+        console.log(`addStravaActivity. Athlete Id ${user.stravaAthleteId},displayName: ${user.displayName},challengeUid: ${user.challengeUid},teamName: ${user.teamName},strava ActivyId: ${stravaActivity.id},Type: ${stravaActivity.type.toLowerCase()},Time: ${new Date(stravaActivity.start_date)}`);
+
+        switch (stravaActivity.type.toLowerCase()) {
+            case "swim":
+                stravaActivityType = "Swim";
+                stravaActivityDistanceUnits = "Yards";
+                //convert meters to yards
+                stravaActivityDistance = stravaActivity.distance * 1.09361;
+                break;
+            case "ride":
+                stravaActivityType = "Bike";
+                // meters to miles
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+                break;
+            case "virtualride":
+                stravaActivityType = "Bike";
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+                break;
+            case "handcycle":
+                stravaActivityType = "Bike";
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+                break;
+            case "run":
+                stravaActivityType = "Run";
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+                break;
+            case "virtualrun":
+                stravaActivityType = "Run";
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+                break;
+            case "walk":
+                stravaActivityType = "Run";
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+                break;
+            default:
+                stravaActivityType = "Other";
+                stravaActivityDistance = stravaActivity.distance * 1.09361 / 1760;   
+        }
+
+        stravaActivityDistance = Math.round((stravaActivityDistance + Number.EPSILON) * 100) / 100
+
+        console.log(`In addStravaActivity with userid ${user.id}, stravaActivity.id: ${stravaActivity.id}`);
+        
+        let activity:Activity = new Activity();
+        // Use strava info as key and use db.set vs add so as not to duplicate activities
+        const activityKey = `${stravaActivity.athlete.id}-${stravaActivity.id}`;
+        activity = {
+            id: activityKey,
             activityDateTime: new Date(stravaActivity.start_date),
             activityName: stravaActivity.name,
             activityType: stravaActivityType,
@@ -59,36 +70,69 @@ const addStravaActivity = ((user : any, stravaActivity : any) => {
             distance: stravaActivityDistance,
             distanceUnits: stravaActivityDistanceUnits,
             duration: stravaActivity.elapsed_time / 3600,
+            durationUnits: "Hours",
             email: user.email ? user.email : "",
             stravaActivity : true,
             stravaActivityId : stravaActivity.id,
             teamName: user.teamName ? user.teamName : "",
             teamUid: user.teamUid ? user.teamUid : null,
-            uid: user.id,
+            uid: user.id
         }
-        if (user.challengeUid) {
-            activity.challengeUid = user.challengeUid;
-        } else {
-            reject(`Error - user: ${user.displayName} is not in a challenge, can not add acvitity`)
-        }
-    
-        console.log(`In addStravaActivity with userid ${user.id}, stravaActivity.id: ${stravaActivity.id}`);
 
-        const dbActivitiesRef = admin.firestore().collection(APP_CONFIG.ORG).doc(APP_CONFIG.ENV).collection("challenges").doc(user.challengeUid).collection(`activities`);
-
-        // USe set so as not to duplicste activities
-        // PAULTODO : --- Addd check to ensure activity is in challenge date range -- if not dump to user activity collection
-        const activityKey = `${stravaActivity.athlete.id}-${stravaActivity.id}`;
-        dbActivitiesRef.doc(activityKey).set(activity).then(() => {
-            console.log(`Firestore activity successfully added with id ${activityKey} for user: ${user.displayName}`);
-            resolve();
-        }).catch((err : any) => {
-            console.error(`Firestore activity add failed`);
-            reject(`Firestore activity add failed with error: ${err}`);
+        // now add to user activities and challenge activities
+        const activityDB = new ActivityDB();
+        activityDB.setUserActivity(activity).then(() => {
+            console.log(`Firestore activity successfully saved with id ${activity.id} for and under user: ${user.displayName}`);
+            // TODO: - actually look for this in array of challenges for this user
+            addActivityToChallenge(user, activity).then(() => {
+                resolve()
+            }).catch((err2 : Error) => {
+                reject(err2);
+            });
+        }).catch((err : Error) => {
+            // Still try to add to challenge - REFACTOR later 
+            console.error(`Firestore activity save for user in user/activities failed, still trying to save to challenge`);
+            addActivityToChallenge(user, activity).then(() => {
+                resolve()
+            }).catch((err1 : Error) => {
+                reject(err1);
+            });
         });
 
     }); // Promise
-    
+});
+
+const addActivityToChallenge = ((user : User, activity : Activity) => {
+    return new Promise((resolve, reject) => {
+        // TODO: - actually look for this in array of challenges for this user
+        if (user.challengeUid) {
+            activity.challengeUid = user.challengeUid;
+            
+            const challengeDB = new ChallengeDB();
+            challengeDB.get(user.challengeUid).then((challenge: Challenge) => {
+                // if valid date, store in challenge activities
+                if (activity.activityDateTime >= challenge.startDate && activity.activityDateTime <= challenge.endDate) {
+                    const activityDB = new ActivityDB();
+                    activityDB.set(activity).then(() => {
+                        console.log(`Firestore activity successfully added with id ${activity.id} for user: ${user.displayName}`);
+                        resolve();
+                    }).catch((err : any) => {
+                        console.error(`Firestore activity add failed`);
+                        reject(`Firestore activity add failed with error: ${err}`);
+                    });
+                } else {
+                    console.log(`Info message - Firestore activity out of challenge range`);
+                    resolve();
+                }
+            }).catch((err1: Error) => {
+                console.error(`Warninng - Cant get challenge for user so activity not added to challenge: ${err1}`);
+                resolve();
+            });
+        } else {
+            console.log(`Warning message - User is not in a challenge so activity not added to challenge`);
+            resolve();
+        }
+    }); // Promise
 });
 
 export { addStravaActivity };
